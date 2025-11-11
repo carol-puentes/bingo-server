4a70361b9cf387fab378bcc444d4397bf71115be
carol-puentes <122763536+carol-puentes@users.noreply.github.com>
2025-11-10 22:05:46 -0500

    Add Netlify app to CORS allowed origins

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get("/", (req, res) => res.send("Bingo server is running"));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://bingo-emaus.netlify.app"],],
+    
     methods: ["GET", "POST"],
   },
 });
