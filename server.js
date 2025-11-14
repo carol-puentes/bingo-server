3c60710a60c6077b188e17c88eb3eda09aecc055
carol-puentes <122763536+carol-puentes@users.noreply.github.com>
2025-11-13 22:19:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.get("/", (req, res) => res.send("Bingo server is running"));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://bingo-emaus.netlify.app"
+    ],
     methods: ["GET", "POST"],
+    credentials: true
   },
 });
+
 
 const PORT = process.env.PORT || 3000;
 
